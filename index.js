8b13c5f8e3fc6c373b9fb66bb96fb8a7fdb3abe7
treuther <tracyreuther@gmail.com>
2019-12-27 13:28:38 -0600

    Done.

--- index.js
+++ index.js
@@ -21,3 +21,14 @@ function titleCased() {
     }).join(' ');
   });
 }
+
+//LAB SOLUTION
+// function titleCased() {
+//   return tutorials.map(function (tutorial) {
+//     tutorial = tutorial.split(' ');
+//     for (var i = 0; i < tutorial.length; i++) {
+//       tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+//     }
+//     return tutorial.join(' ');
+//   });
+// }
