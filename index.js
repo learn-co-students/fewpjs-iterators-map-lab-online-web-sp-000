9812ccd3acfeaee5d58a0efb0378652962f38fe6
stabassum <sumu1993@gmail.com>
2020-01-05 17:10:40 -0500

    Done.

--- index.js
+++ index.js
@@ -9,9 +9,14 @@ const tutorials = [
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
-  /
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(function (tutorial) {
+    tutorial = tutorial.split(' ');
+    for (var i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+    }
+    return tutorial.join(' ');
+  });
 }
