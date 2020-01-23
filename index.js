4ad56016b1b97f88f3ba6328d0a62b213c7dcabf
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-01-23 23:09:29 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+
+function titleCased(tutorials) {
+   var splitStr = tutorials.toLowerCase().split(' ');
+   for (var i = 0; i < splitStr.length; i++) {
+       // You do not need to check if i is larger than splitStr length, as your for does that for you
+       // Assign it back to the array
+       splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+   }
+   // Directly return the joined string
+   return splitStr.join(' ');
 }
