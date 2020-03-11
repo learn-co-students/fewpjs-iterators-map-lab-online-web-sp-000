127cae79f02a4f4cb9729020cc31f2b109f4062a
cperez1012 <perezchristian1012@gmail.com>
2020-03-11 16:43:20 -0400

    Done.

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ const tutorials = [
 ];
 
 function titleCased() {
-  return tutorials.map( function(tutorial) {
-    return tutorial.split(' ').map(function(word) {
-      return (word.charAt(0).toUpperCase() + word.slice(1));
-    })
-    .join(' ');
-  })
-};
+  return tutorials.map(function (tutorial) {
+    tutorial = tutorial.split(' ');
+    for (var i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+    }
+    return tutorial.join(' ');
+  });
+}
