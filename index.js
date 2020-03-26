8f1769af18255bbd067de9ddd741bbb8df1baa0a
BlythePollard <github email address>
2020-03-25 18:29:14 -0600

    Done.

--- index.js
+++ index.js
@@ -13,11 +13,10 @@ const tutorials = [
 
 const titleCased = () => {
   return tutorials.map(function(sentence) {
-    let newSentence = sentence.split(' ') //why no let or const?
+    let newSentence = sentence.split(' ') 
     for(var i = 0; i < newSentence.length; i++) {
       newSentence[i] = newSentence[i].charAt(0).toUpperCase() + newSentence[i].substring(1)
-      //first letter of each item capitalized
-    }
+]    }
     return newSentence.join(' ')
   })
 }
