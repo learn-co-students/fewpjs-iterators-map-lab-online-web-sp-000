808e65232e40f468a0a9940f4cf55a16d00bff5c
zdsmit <zdsmith001@gmail.com>
2020-05-17 08:31:58 -0700

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,14 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(title) {
+    let words = title.split(" ")
+    //words.forEach(word => {
+      //word.charAt(0).toUpperCase() + word.slice(1)
+    //})
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1)
+    }
+    return words.join(" ")
+  })
 }
