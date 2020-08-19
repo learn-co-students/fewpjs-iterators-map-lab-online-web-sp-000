e259a6f2c4a053621df2d6e3de22ab2727a35bc8
Chris Kay <ckay84@protonmail.com>
2020-08-19 18:36:48 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+const titleCased = function() {
+  return tutorials.map(tutorial => {
+    let words = tutorial.split(' ');
+    let mappedWords = words.map(function(word) {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    console.log(mappedWords);
+    let joinedWords = mappedWords.join(' ');
+    console.log(joinedWords);
+    return joinedWords;
+  })
 }
