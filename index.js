a3eae6810e68abf4107ad17e14c9d5a6087ac32e
wkdewey <wkdewey@gmail.com>
2020-08-30 16:50:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Contutorialuctor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+  let result = tutorials.map((tutorial) => makeTitleCase(tutorial));
+  return result;
+};
+
+let makeTitleCase = (tutorial) => {
+  let eachWord = tutorial.split(" ");
+  let final = eachWord.map((word) =>
+    word.replace(word[0], word[0].toUpperCase())
+  );
+  return final.join(" ");
+};
