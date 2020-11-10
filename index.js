11b97f983d3cc954f08213caf27b00ea98dec49a
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-11-10 17:46:21 +0000

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(str) {
+  return str.toLowerCase().split(' ').map(function(word) {
+    return (word.charAt(0).toUpperCase() + word.slice(1));
+  }).join(' ');
 }
+
+tutorials.map(function(e){
+    return titleCased(e);
+});
