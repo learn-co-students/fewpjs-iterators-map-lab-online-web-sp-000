a486f4facd3d4e5b2083e4daad855564370a7459
zchesny <z.chesny@gmail.com>
2020-12-30 11:44:24 -0700

    Done.

--- index.js
+++ index.js
@@ -18,3 +18,15 @@ const titleCased = () => {
     return newArr.join(' '); 
   })
 }
+
+/* //their solution
+function titleCased() {
+  return tutorials.map(function (tutorial) {
+    tutorial = tutorial.split(' ');
+    for (var i = 0; i < tutorial.length; i++) {
+      tutorial[i] = tutorial[i].charAt(0).toUpperCase() + tutorial[i].slice(1);
+    }
+    return tutorial.join(' ');
+  });
+}
+*/
