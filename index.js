a28de5b9f10fbda7342baeed73213ecb8a9dae62
AustinRhoads <austinrrhoads@ymail.com>
2021-01-31 12:50:45 -0600

    8-bit universe

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+let titleCased = () => {
+    
+  return tutorials.map((lesson) =>
+   lesson.split(" ").map((word) =>
+    word.charAt(0).toUpperCase() + word.slice(1)).join(" "))
 }
