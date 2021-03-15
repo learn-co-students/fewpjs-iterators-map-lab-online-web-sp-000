00e85720007f20f088c623e3fa848ca29d898d8e
yarvinh <yarvinhhernandez>
2021-03-15 00:17:53 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(){
+  return tutorials.map(function(e){ 
+   let words = e.split(' ').map(function(word){
+        return word.charAt(0).toUpperCase() + word.slice(1)
+    })
+    return words.join(' ')
+  })
 }
+
+
