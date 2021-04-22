0d49e602a9ca98d25457cf64fdc88213f5a8a57f
deliaconstantino <dconstan92@gmail.com>
2021-04-22 17:20:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Contutorialuctor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Contutorialuctor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
+  let answer = tutorials.map((line) => {
+    let sentence = line.split(" ");
+    let newSentence = sentence.map((word) => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+    return newSentence.join(" ");
+  });
+  return answer;
 }
+
+
+// const titleCased = () => {
+//   return tutorials.map((sentence) => {
+//     return sentence
+//       .split(" ")
+//       .map((word) => word[0].toUpperCase() + word.slice(1))
+//       .join(" ");
+//   });
+// }
